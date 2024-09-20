5f0873de6fe78948672d63182ec947b9ca4ce264
Dineshmutukuri <122514562+Dineshmutukuri@users.noreply.github.com>
2024-09-20 12:45:25 +0530

    find sum.cpp

---
+++ find sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int sum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    int size;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
+
+    int arr[size];
+    std::cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        std::cin >> arr[i];
+    }
+
+    int total = sum(arr, size);
+    std::cout << "The sum of the array elements is: " << total << std::endl;
+
+    return 0;
+}
