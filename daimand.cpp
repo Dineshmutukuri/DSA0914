d974cb130149a9787c1d16c39ef6d4b80f469fa4
Dineshmutukuri <122514562+Dineshmutukuri@users.noreply.github.com>
2024-09-20 12:50:52 +0530

    daimand.cpp

---
+++ daimand.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    int rows;
+    std::cout << "Enter the number of rows: ";
+    std::cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = rows - 1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
